0f77065cd935e420c6b4a691e47d61b7441a5868
Jose David Pacheco Valedo <josepdvahn@me.com>
2013-09-25 09:36:56 -0500

    Partial Build

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,6 +62,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def relationships
+    @allItems = Item.all
+    @item = Item.find(params[:id])
+    render 'relation_manager'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
